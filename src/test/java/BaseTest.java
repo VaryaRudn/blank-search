2a6fb2897480aec3c0ed75817de0dd610bd2483d
Varya <vararudneva@gmail.com>
2025-05-06 15:40:27 +0300

    браузер

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,17 +1,14 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.Condition;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+
 
 abstract public class BaseTest {
 
     public void setUp() {
         // Открываем нужный сайт
-        WebDriverManager.chromedriver().setup();
+
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.headless = true;
@@ -22,6 +19,7 @@ abstract public class BaseTest {
     public void init(){
         setUp();
     }
+
     @AfterEach
     public void tearDown() {
         // Закрываем браузер после теста
