6a7153a15ec9045ce9d125fea81d777d2d57098e
Varya <vararudneva@gmail.com>
2025-05-06 14:53:11 +0300

    ошибка с провайдерами исправлена

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.codeborne:selenide:7.9.1")
+    implementation ("ch.qos.logback:logback-classic:1.4.14")
 }
 
 tasks.test {
