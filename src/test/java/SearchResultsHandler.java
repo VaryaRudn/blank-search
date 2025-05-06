6aade634ee5db833ceab8e84dd1a28f89b7b8120
Varya <vararudneva@gmail.com>
2025-05-06 15:04:04 +0300

    изменены селекторы

--- src/test/java/SearchResultsHandler.java
+++ src/test/java/SearchResultsHandler.java
@@ -1,23 +1,18 @@
-import org.openqa.selenium.By;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 
 public class SearchResultsHandler extends BaseTest {
 
-    private By searchButton = By.cssSelector("button.search__button");
-    private By resultCount = By.cssSelector("div.text-2.page-lead__search-result-count");
-
     public void clickSearchButton() {
-        $(searchButton).click(); // Нажимаем на кнопку поиска
+        $("button.search__button").click();
     }
 
     public int getResultCount() {
-        String resultText = $(resultCount).getText(); // Получаем текст с количеством результатов
+        String resultText = $("div.text-2.page-lead__search-result-count").getText();
 
-        // Проверяем, что текст не пустой и содержит ожидаемое значение
         if (resultText.isEmpty() || !resultText.matches("\\d+ результатов")) {
-            return 0; // Если текст пустой или не соответствует формату, возвращаем 0
+            return 0;
         }
 
-        return Integer.parseInt(resultText.split(" ")[0]); // Извлекаем число из текста
+        return Integer.parseInt(resultText.split(" ")[0]);
     }
 }
