efc9bfc6b3a0f0199afa44aca2e66d75acec7507
Varya <vararudneva@gmail.com>
2025-05-06 15:35:15 +0300

    браузер

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,16 +1,27 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.Condition;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 
-public class BaseTest {
-    @BeforeEach
+abstract public class BaseTest {
+
     public void setUp() {
         // Открываем нужный сайт
+        WebDriverManager.chromedriver().setup();
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.headless = true;
         Selenide.open("https://severstal.com/rus/");
     }
 
+    @BeforeEach
+    public void init(){
+        setUp();
+    }
     @AfterEach
     public void tearDown() {
         // Закрываем браузер после теста
