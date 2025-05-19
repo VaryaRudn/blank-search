973ac72ff83ed443a56f6e3b81bbfb6cdac5d2b7
Varya <vararudneva@gmail.com>
2025-05-19 23:00:07 +0300

    first commit

--- src/test/java/api/ApiTest.java
+++ src/test/java/api/ApiTest.java
@@ -2,18 +2,18 @@ package api;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApiTest {
-    public static void main(String[] args) {
 
+    @Test
+    public void testApiEndpoint() {
         String url = "https://jsonplaceholder.typicode.com/posts/1";
         Response response = RestAssured.get(url);
         int statusCode = response.getStatusCode();
-        if (statusCode == 200) {
-            System.out.println("Ссылка доступна: " + url);
-        } else {
-            System.out.println("Ссылка недоступна. Статус-код: " + statusCode);
-            System.exit(1);
-        }
+
+        // Проверяем, что статус-код равен 200
+        assertEquals(200, statusCode, "Ссылка недоступна. Статус-код: " + statusCode);
     }
 }
